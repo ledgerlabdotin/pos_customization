959b7a8ae8bb91f22a67f54a3fbfd4a76f1f7e6f
ledgerlabdotin <85391132+ledgerlabdotin@users.noreply.github.com>
2025-11-17 23:19:49 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
+setup(
+    name="pos_customization",
+    version="0.0.1",
+    description="POS customizations for ERPNext",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
+)
+
