da3993f697e9c2569171a440f903f092bfba73ca
ledgerlabdotin <85391132+ledgerlabdotin@users.noreply.github.com>
2025-11-17 23:25:33 +0300

    Add files via upload

---
+++ pos_customization/hooks.py
@@ -0,0 +1,12 @@
+
+app_name = "pos_customization"
+app_title = "POS Customization"
+app_publisher = "Your Name"
+app_description = "POS customizations for ERPNext POS"
+app_email = "you@example.com"
+app_license = "MIT"
+
+# Load custom JS on the POS page
+page_js = {
+    "point-of-sale": "public/js/pos_print_button.js"
+}
